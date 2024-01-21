74b043ef07d60336de06fa2aae7f2c946d05b888
pawarherschel <pawarherschel@gmail.com>
2024-01-21 23:27:51 +0530

    removed old function

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@
 #![allow(clippy::missing_const_for_fn)]
 
 use std::collections::{BTreeSet, HashMap};
-use std::hash::{BuildHasher, Hasher};
+use std::fs;
+use std::hash::BuildHasher;
 use std::io::Read;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use std::{fs, time};
 
 use indicatif::ProgressIterator;
 use libflate::gzip;
 use prost::Message;
 use ureq::serde_json::Value;
-use ureq::{json, serde_json, Error, Response};
+use ureq::{json, Error, Response};
 
 use tachiyomi_backup_to_anilist::anilist::{get_code, get_token};
 use tachiyomi_backup_to_anilist::responses::{Format, Medum, Root};
@@ -92,196 +92,11 @@ pub fn separator() {
     println!("{}", (0..15).map(|_| '-').collect::<String>());
 }
 
-// 700 milliseconds delay per request
-
-pub fn try_again(name: String) -> Result<Response, ureq::Error> {
-    let variables = json!({
-        "search": name.as_str(),
-        "mediaType": "MANGA"
-    });
-    let json = json!({
-        "query": SEARCH_QUERY,
-        "variables": variables
-    });
-    let resp = ureq::post("https://graphql.anilist.co")
-        .set("Content-Type", "application/json")
-        .set("Accept", "application/json")
-        .send_json(json);
-    resp
-}
-
-#[allow(clippy::too_many_lines)]
-pub fn rate_limited_query(name: String, cache: &mut HashMap<String, Value>) -> Option<u64> {
-    if let Some(data) = cache.get(&name) {
-        let binding = data["data"]["Page"]["media"].clone();
-        let list = binding.as_array().unwrap();
-        let list = list
-            .into_iter()
-            .filter(|it| it["format"] == "MANGA")
-            .collect::<Vec<_>>();
-
-        let found = list.iter().find(|it| {
-            let romaji = &it["title"]["romaji"];
-            let english = &it["title"]["english"];
-            let native = &it["title"]["native"];
-            let user_preferred = &it["title"]["user_preferred"];
-            match &name {
-                romanji => true,
-                english => true,
-                native => true,
-                user_preferred => true,
-                _ => false,
-            }
-        });
-
-        return found.map(|m| m["id"].as_number().unwrap().as_u64().unwrap());
-    }
-
-    let variables = json!({
-        "search": name.as_str(),
-        "mediaType": "MANGA"
-    });
-    let json = json!({
-        "query": SEARCH_QUERY,
-        "variables": variables
-    });
-    let resp = ureq::post("https://graphql.anilist.co")
-        .set("Content-Type", "application/json")
-        .set("Accept", "application/json")
-        .send_json(json.clone());
-
-    let mut now = time::Instant::now();
-
-    let resp = match resp {
-        Ok(resp) => resp,
-        Err(e) => match e {
-            Error::Status(429, r) => {
-                let headers = r.headers_names();
-                for header in headers {
-                    let header_details = r.header(header.as_str());
-                    println!("{header}: {header_details:?}");
-                }
-                let retry_after = r.header("retry-after").unwrap().parse().unwrap();
-                eprintln!("rate limit exceeded: {}", r.into_json::<Value>().unwrap());
-                sleep(Duration::from_secs(retry_after));
-                return rate_limited_query(name, cache);
-            }
-            Error::Status(s, r) => {
-                let headers = r.headers_names();
-                let headers = headers
-                    .iter()
-                    .map(|it| r.header(it.as_str()))
-                    .map(|it| it.unwrap_or_default().to_string())
-                    .collect::<Vec<_>>();
-
-                println!(
-                    "response: {}\n\
-                    headers: {:?}\n\
-                    json: {:?}\n\
-                    ---- retrying",
-                    r.into_json::<Value>().unwrap(),
-                    headers,
-                    json
-                );
-                let elapsed = now.elapsed();
-                let sleep_time = Duration::from_secs_f32(2.0).saturating_sub(elapsed);
-
-                if sleep_time < Duration::from_millis(500) {
-                    println!("{headers:?}");
-                    println!("slept for {sleep_time:?}");
-                }
-
-                sleep(sleep_time);
-
-                now = Instant::now();
-
-                let response = try_again(name.clone());
-
-                match response {
-                    Ok(r) => {
-                        println!("!!!!worked");
-                        r
-                    }
-                    Err(e) => match e {
-                        Error::Status(500, _) => return None,
-                        Error::Status(s, r) => {
-                            panic!("different error fml\n{}", r.into_json::<Value>().unwrap())
-                        }
-                        Error::Transport(_) => {
-                            panic!()
-                        }
-                    },
-                }
-            }
-            Error::Transport(e) => {
-                panic!("{e}");
-            }
-        },
-    };
-
-    let headers = resp.headers_names();
-    let headers = headers
-        .into_iter()
-        .map(|it| {
-            (
-                it.clone(),
-                resp.header(it.as_str()).unwrap_or_default().to_string(),
-            )
-        })
-        .collect::<Vec<_>>();
-
-    let ratelimit_remaining = resp
-        .header("x-ratelimit-remaining")
-        .unwrap_or_default()
-        .parse()
-        .unwrap_or(0_u32);
-
-    if ratelimit_remaining < 30 {
-        println!("x-ratelimit-remaining: {ratelimit_remaining}",);
-    }
-
-    let data = resp.into_json::<Value>().unwrap();
-    let binding = data["data"]["Page"]["media"].clone();
-    let list = binding.as_array().unwrap();
-    let list = list
-        .into_iter()
-        .filter(|it| it["format"] == "MANGA")
-        .collect::<Vec<_>>();
-
-    let found = list.iter().find(|it| {
-        let romaji = &it["title"]["romaji"];
-        let english = &it["title"]["english"];
-        let native = &it["title"]["native"];
-        let user_preferred = &it["title"]["user_preferred"];
-        match &name {
-            romanji => true,
-            english => true,
-            native => true,
-            user_preferred => true,
-            _ => false,
-        }
-    });
-
-    cache.insert(name, data);
-
-    let elapsed = now.elapsed();
-    let sleep_time = Duration::from_secs_f32(2.0).saturating_sub(elapsed);
-
-    if sleep_time < Duration::from_millis(500) {
-        println!("{headers:?}");
-        println!("slept for {sleep_time:?}");
-    }
-
-    sleep(sleep_time);
-
-    found.map(|m| m["id"].as_number().unwrap().as_u64().unwrap())
-}
-
 const MAX_RETRIES: u8 = 5;
 
-pub fn rate_limited_query_new(
+pub fn rate_limited_query(
     name: String,
-    cache: &mut HashMap<String, Vec<Medum>, impl Hasher + BuildHasher>,
+    cache: &mut HashMap<String, Vec<Medum>>,
     try_num: Option<u8>,
 ) -> Option<u64> {
     if let Some(media) = cache.get(&name) {
@@ -299,7 +114,7 @@ pub fn rate_limited_query_new(
                 let retry_after = r.header("retry-after").unwrap().parse().unwrap();
                 eprintln!("rate limit exceeded");
                 sleep(Duration::from_secs(retry_after));
-                return rate_limited_query_new(name, cache, Some(try_num.unwrap_or_default()));
+                return rate_limited_query(name, cache, Some(try_num.unwrap_or_default()));
             }
             Error::Status(500, r) => {
                 eprintln!("bad request");
@@ -307,7 +122,7 @@ pub fn rate_limited_query_new(
                 if try_num.is_some_and(|num| num > MAX_RETRIES) {
                     return None;
                 }
-                return rate_limited_query_new(name, cache, Some(try_num.unwrap_or_default()));
+                return rate_limited_query(name, cache, Some(try_num.unwrap_or_default()));
             }
             Error::Status(status_code, r) => {
                 if try_num.is_some_and(|num| num > MAX_RETRIES) {
@@ -406,24 +221,6 @@ pub fn find_id(name: &str, media: &[Medum]) -> Option<u64> {
     found.map(|m| m.id)
 }
 
-pub fn migrate_cache(mut old_cache: HashMap<String, Value>) -> HashMap<String, Vec<Medum>> {
-    old_cache
-        .into_iter()
-        .map(|(k, v)| {
-            (k, {
-                match serde_json::from_value::<Root>(v.clone()) {
-                    Ok(v) => v,
-                    Err(e) => {
-                        println!("{}", serde_json::to_string_pretty(&v).unwrap());
-                        panic!("{e:#?}")
-                    }
-                }
-            })
-        })
-        .map(|(k, v)| (k, v.data.page.media))
-        .collect()
-}
-
 #[allow(clippy::too_many_lines)]
 fn main() {
     let tachibk = fs::read("assets/backup.tachibk").unwrap();
@@ -483,7 +280,7 @@ fn main() {
             .map(|it| {
                 (
                     it.clone(),
-                    rate_limited_query(it.title().to_string(), &mut cache),
+                    rate_limited_query(it.title().to_string(), &mut cache, None),
                 )
             })
             .filter(|(_, it)| it.is_none())
@@ -498,7 +295,7 @@ fn main() {
             .map(|it| {
                 (
                     it.clone(),
-                    rate_limited_query(it.title().to_string(), &mut cache),
+                    rate_limited_query(it.title().to_string(), &mut cache, None),
                 )
             })
             .filter(|(_, it)| it.is_some())
@@ -526,7 +323,7 @@ fn main() {
     not_tracked
         .iter()
         .map(|it| (it, it.title()))
-        .map(|(m, it)| (m, it, rate_limited_query(it.to_string(), &mut cache)))
+        .map(|(m, it)| (m, it, rate_limited_query(it.to_string(), &mut cache, None)))
         .for_each(|(m, n, it)| {
             it.map_or_else(
                 || untracked_none.push((n, m)),
@@ -550,7 +347,7 @@ fn main() {
     tracked
         .iter()
         .map(|it| (it, it.title()))
-        .map(|(m, it)| (m, it, rate_limited_query(it.to_string(), &mut cache)))
+        .map(|(m, it)| (m, it, rate_limited_query(it.to_string(), &mut cache, None)))
         .for_each(|(m, n, it)| {
             it.map_or_else(
                 || tracked_none.push((n, m)),
@@ -565,17 +362,6 @@ fn main() {
 
     write_items_to_file!(tracked_some);
     write_items_to_file!(tracked_none);
-
-    separator();
-
-    let mut new_cache = migrate_cache(cache.clone());
-
-    dbg!(cache.len());
-    dbg!(new_cache.len());
-
-    separator();
-
-    write_items_to_file!(new_cache);
 
     separator();
 
